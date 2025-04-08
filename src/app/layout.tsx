9b787de95aa2395a457b3dad2e845ccca38d1c84
Seun Oyediran <seunoyediran22517@gmail.com>
2025-04-08 10:54:36 +0100

    og image fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,13 @@ import "../../public/scss/main.scss";
 import Provider from "@/state/provider";
 import { Loader } from "@react-three/drei";
 
+const title = "Chair Configurator";
+const description =
+  "Design your perfect chair in real-time with our 3D configurator. Customize colors, materials, and finishes to match your style.";
+
 export const metadata: Metadata = {
-  title: "Chair Configurator",
-  description:
-    "Design your perfect chair in real-time with our 3D configurator. Customize colors, materials, and finishes to match your style.",
+  title,
+  description,
 };
 
 export const viewport: Viewport = {
@@ -54,6 +57,28 @@ export default function RootLayout({
           />
 
           <link rel="shortcut icon" href="/favicon/favicon.ico" />
+
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="2400" />
+          <meta property="og:image:height" content="1260" />
+          <meta
+            property="og:image"
+            content="/favicon/opengraph-image.png"
+          ></meta>
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:description" content={description} />
+          <meta property="twitter:image:type" content="image/png" />
+          <meta property="twitter:image:width" content="2400" />
+          <meta property="twitter:image:height" content="1260" />
+
+          <meta
+            name="twitter:image"
+            content="/favicon/twitter-image.png"
+          ></meta>
         </head>
         <body>{children}</body>
       </html>
